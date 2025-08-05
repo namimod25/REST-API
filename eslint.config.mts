85bd3bc1e24aeea215eaf1eb3f7b42b9cae540dd
dodit2819 <namimod25@gmail.com>
2025-08-05 21:22:17 +0700

    Setup Eslint, Prettier, Husky

--- eslint.config.mts
+++ eslint.config.mts
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from '@eslint/js';
+import globals from 'globals';
 
-import { defineConfig } from 'eslint/config'
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } }
-  }
-])
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express, { Application, Request, Response } from 'express';
 
-const app: Application = express()
-const port: Number = 3232
+const app: Application = express();
+const port: Number = 3232;
 
-app.use('/koneksi', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Hello World' })
-})
-app.listen(port, () => console.log(`server is running on port ${port}`))
+app.use('/healt', (_req: Request, res: Response) => {
+  res.status(200).send({ data: 'hello' });
+});
+
+app.listen(port, () => console.log(`Server berjalan port ${port}`));
+
+export default app;
