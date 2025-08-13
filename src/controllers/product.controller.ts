3d80fe45023494fa5bc7e39dbad3212544810dff
dodit2819 <namimod25@gmail.com>
2025-08-13 15:49:24 +0700

    modif controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { createProductValidation } from '../validation/product.validation';
 import { logger } from '../utils/logger';
+import { getProductFromDB } from '../services/product.service';
 
 export const createProduct = (req: Request, res: Response) => {
   const { error, value } = createProductValidation(req.body);
@@ -22,31 +23,30 @@ export const createProduct = (req: Request, res: Response) => {
   });
 };
 
-export const getProduct = (req: Request, res: Response) => {
-  const product = [
-    { name: 'sepatu', price: 150000 },
-    { name: 'Baju polosan', price: 10000 },
-  ];
-  const {
-    params: { name },
-  } = req;
-  if (name) {
-    const filterProducts = product.filter((product) => {
-      if (product.name === name) {
-        return product;
-      }
-    });
-    if (filterProducts.length === 1) {
-      logger.info('Data Not Found');
-      return res
-        .status(404)
-        .send({ status: false, statusCode: 404, data: filterProducts[0] });
-    }
-  }
+export const getProduct = async (req: Request, res: Response) => {
+  const products = await getProductFromDB();
+
   logger.info('success get data');
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    data: product,
-  });
+  return res
+    .status(200)
+    .send({ status: true, statusCode: 200, data: products });
+
+  //   const {
+  //     params: { name },
+  //   } = req;
+
+  //   if (name) {
+  //     const filterProducts = products.filter((product) => {
+  //       if (product.name === name) {
+  //         return product;
+  //       }
+  //     });
+  //     if (filterProducts.length === 1) {
+  //       logger.info('Data Not Found');
+  //       return res
+  //         .status(404)
+  //         .send({ status: false, statusCode: 404, data: filterProducts[0] });
+  //     }
+  //   }
+  //
 };
